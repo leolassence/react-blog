1f462f71d676afea302ace0f7c9ddb964b9236a9
leolassence <leolassence@gmail.com>
2020-01-14 13:54:55 +0100

    Add from validation

--- src/containers/PostForm.js
+++ src/containers/PostForm.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { reduxForm, Field } from 'redux-form';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -7,9 +6,45 @@ import history from '../history';
 
 import { createPost } from '../actions';
 
-const formConfig = {
-  form: 'createPostForm'
-};
+const validate = values => {
+  const errors = {}
+
+  if (!values.title) {
+    errors.title = 'Required'
+  } else if (values.title.length > 10) {
+    errors.title = 'Must be 10 characters or less'
+  }
+
+  if (!values.content) {
+    errors.content = 'Required'
+  } else if (values.content.length > 15) {
+    errors.content = 'Must be 15 characters or less'
+  }
+
+  if (!values.author) {
+    errors.author = 'Required'
+  }
+
+  return errors
+}
+
+const renderField = ({ input, label, type, meta: { touched, error } }) => {
+  let field = <input {...input} placeholder={label} type={type} className='form-control' />
+
+  if (type === 'textarea') {
+    field = <textarea cols='3' rows='5' {...input} placeholder={label} type={type} className='form-control' />
+  }
+
+  const spanError = touched && error ? <span>{error}</span> : '';
+
+  return (
+    <div className={`form-group ${touched && error ? 'has-danger' : ''}`}>
+      <label>{label}</label>
+      {field}
+      {spanError}
+    </div>
+  )
+}
 
 class PostForm extends React.Component {
   createPost(post) {
@@ -18,40 +53,21 @@ class PostForm extends React.Component {
   }
 
   render() {
-    const { handleSubmit } = this.props;
+    const { handleSubmit, submitting } = this.props;
 
     return (
       <div>
         <h1>New Post</h1>
         <form onSubmit={handleSubmit(this.createPost.bind(this))}>
-          <div className='form-group'>
-            <label htmlFor='title'>Titre</label>
-            <Field
-              name='title'
-              component='input'
-              className='form-control'
-              type='text' />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='content'>Content</label>
-            <Field
-              name='content'
-              component='textarea'
-              className='form-control'
-              type='textarea' />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='author'>Author</label>
-            <Field
-              name='author'
-              component='input'
-              className='form-control'
-              type='text' />
-          </div>
-          <Link to={'/'} className='button-space'>
-            <button className='btn btn-danger'>Retour</button>
-          </Link>
-          <button className='btn btn-primary' type='submit'>Submit</button>
+          <Field name='title' type='text' component={renderField} label='Title'/>
+          <Field name='content' type='textarea' component={renderField} label='Content'/>
+          <Field name='author' type='text' component={renderField} label='Author'/>
+          <button
+            disabled={toString(submitting)}
+            className='btn btn-primary'
+            type='submit'>
+            Submit
+          </button>
         </form>
       </div>
     );
@@ -62,5 +78,9 @@ const mapDispatchToProps = (dispatch) => ({
   ...bindActionCreators({ createPost }, dispatch)
 });
 
+const formConfig = {
+  form: 'createPostForm',
+  validate
+};
 
 export default connect(null, mapDispatchToProps)(reduxForm(formConfig)(PostForm));
